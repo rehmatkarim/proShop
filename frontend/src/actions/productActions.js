7f0a883385b25d0b93eca259cdfde28474af2c6b
rehmatkarim <p146120@gmail.com>
2021-04-13 16:28:46 -0400

    redux is set for both homescreen and product details screen

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,6 +3,9 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL
 } from "../constants/productConstants";
 
 export const listProducts = () => async (dispatch) => {
@@ -23,3 +26,22 @@ export const listProducts = () => async (dispatch) => {
     });
   }
 };
+
+export const listProductDetails = (id) => async (dispatch) => {
+    try {
+      dispatch({ type: PRODUCT_DETAILS_REQUEST });
+      const { data } = await axios.get(`/api/products/${id}`);
+      dispatch({
+        type: PRODUCT_DETAILS_SUCCESS,
+        payload: data,
+      });
+    } catch (e) {
+      dispatch({
+        type: PRODUCT_DETAILS_FAIL,
+        payload:
+          e.response && e.response.data.message
+            ? e.response.data.message
+            : e.message,
+      });
+    }
+  };
