e09837ffd2f5bd99311ade346e3c841f638d1dde
rehmatkarim <p146120@gmail.com>
2021-04-13 13:49:32 -0400

    middleware for error is set

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,9 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import colors from "colors";
 import productRoutes from "./routes/productRoutes.js";
+import bodyParser from "body-parser";
+import Product from "./models/productModel.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
@@ -15,6 +18,8 @@ app.get("/", (req, res) => {
   res.send("Api is running");
 });
 app.use("/api/products", productRoutes);
+app.use(notFound);
+app.use(errorHandler );
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
