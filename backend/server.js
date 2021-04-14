ef4bfb536a33ad2e5543f288f0559126baf73238
rehmatkarim <p146120@gmail.com>
2021-04-14 15:55:30 -0400

    authentication endpoint is done

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import colors from "colors";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import bodyParser from "body-parser";
 import Product from "./models/productModel.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
@@ -13,13 +14,15 @@ dotenv.config();
 
 connectDB();
 const app = express();
+app.use(express.json()); 
 
 app.get("/", (req, res) => {
   res.send("Api is running");
 });
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 app.use(notFound);
-app.use(errorHandler );
+app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
