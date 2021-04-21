41838c1688496e0ba61052b782dd216762a9f590
rehmatkarim <p146120@gmail.com>
2021-04-21 12:16:31 -0400

    update profile endpoint created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,31 +25,30 @@ const authUser = asyncHandler(async (req, res) => {
 //@route POST /api/users
 //@access Public
 const registerUser = asyncHandler(async (req, res) => {
-    const { name,email, password } = req.body;
-    const userExist = await User.findOne({ email });
-   if (userExist){
-       res.status(400);
-       throw new Error("User already exist");
-   }
-   const user = await User.create({
-       name,
-       email,
-       password
-   })
-   if (user){
-       res.status(201).json({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        isAdmin: user.isAdmin,
-        token: generateToken(user._id),
-       })
-   }else {
-       res.status(400);
-       throw new Error("Invalid User")
-   }
+  const { name, email, password } = req.body;
+  const userExist = await User.findOne({ email });
+  if (userExist) {
+    res.status(400);
+    throw new Error("User already exist");
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
   });
-  
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid User");
+  }
+});
 
 //@desc Get user profile
 //@route GET /api/users/profile
@@ -69,5 +68,29 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
+//@desc UPDATE user profile
+//@route PUT /api/users/profile
+//@access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updateUser = await user.save();
+    res.json({
+      _id: updateUser._id,
+      name: updateUser.name,
+      email: updateUser.email,
+      isAdmin: updateUser.isAdmin,
+      token: generateToken(updateUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
 
-export { authUser, getUserProfile,registerUser };
+export { authUser, getUserProfile, registerUser,updateUserProfile };
